cd4816a74e76bf15fd168bbc55bc701a6b9b43cd
Student <example@example.com>
2020-02-17 14:14:01 -0800

    add onDragOver function and comments for use in html and main.js

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -1,14 +1,24 @@
-function onDragStart(event) {
+//Require listener for Items -> ondragstart="return onDragStart(event)";
+export function onDragStart(event) {
   event.dataTransfer.setData('text', event.target.id);
-  //event.target.className += hold; to add a temp visuals
+  //event.target.className += ' hold'; to add a temp visuals
+  //this instead of event.target may work
   //setTImeout(() => (event.target.className = 'invisible'), 0); will clear the old image out while dragging
 }
 
-function onDragOver(event) {
+//Require listeners for Campers -> ondragover="return onDragOver(event)";
+export function onDragOver(event) {
   event.preventDefault();
 }
 
-function onDrop(event) {
+// -> ondragenter="return onDragEnter(event)";
+export function onDragEnter(event) {
+  event.preventDefault();
+  //this.className += 'empty';
+}
+
+// -> ondrop="onDrop(event)";
+export function onDrop(event) {
   const id = event.dataTransfer.getData('text');
   const draggableElement = document.getElementById(id);
   const dropzone = event.target;
