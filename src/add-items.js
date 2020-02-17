722643d53c99eef25aca6139837b9bde937d406f
Student <example@example.com>
2020-02-17 14:26:58 -0800

    add import statements for local images and update file paths in addKnownItem and addOtherItem functions

--- src/add-items.js
+++ src/add-items.js
@@ -1,4 +1,17 @@
 import $ from 'jQuery';
+import './assets/images/tent.png';
+import './assets/images/firewood.png';
+import './assets/images/booze.png';
+import './assets/images/cards.png';
+import './assets/images/cooler.png';
+import './assets/images/firstaid.png';
+import './assets/images/hatchet.png';
+import './assets/images/lantern.png';
+import './assets/images/stove.png';
+import './assets/images/transport.png';
+import './assets/images/water-filter.png';
+import './assets/images/chair.png';
+import './assets/images/default.png';
 
 export function initializePage() {
   $("form#initialize-trip").submit(function(event) {
@@ -24,9 +37,9 @@ export function addCamper() {
     let camper = $("input#camper").val();
     // Add any necessary clases for drag/drop into the append statement
     if (counter %2 === 0) {
-      $("#left-campers").append(`<div class="card"><h3>${camper}</h3></div>`);
+      $("#left-campers").append(`<div ondrop="onDrop(event)" ondragenter="return onDragEnter(event)" ondragover="return onDragOver(event)" class="card"><h3>${camper}</h3></div>`);
     } else {
-      $("#right-campers").append(`<div class="card"><h3>${camper}</h3></div>`);
+      $("#right-campers").append(`<div ondrop="onDrop(event)" ondragenter="return onDragEnter(event)" ondragover="return onDragOver(event)" class="card"><h3>${camper}</h3></div>`);
     }
     counter += 1;
   });
@@ -36,8 +49,8 @@ export function addKnownItem() {
   $("form#add-known-item").submit(function(event) {
     event.preventDefault();
     let knownItem = $("#known-item").val();
-    let knownImgUrl = `./assets/images/${knownItem}.png`; // Requires figuring out local images.
-    $("#added-items").append(`<div class="card"><h3>${knownItem}</h3><img src="${knownImgUrl}" alt="a photo of an item"></div>`);
+    let knownImgUrl = `assets/images/${knownItem}.png`;
+    $("#added-items").append(`<div ondragstart="return onDragStart(event)" class="card"><h3>${knownItem}</h3><img src="${knownImgUrl}" alt="a photo of an item"></div>`);
   });
 }
 
@@ -45,7 +58,6 @@ export function addOtherItem() {
   $("form#add-other-item").submit(function(event) {
     event.preventDefault();
     let otherItem = $("input#other-item").val();
-    let otherImgUrl = `../img/default.jpeg`; // requires a default img
-    $("#added-items").append(`<div class="card"><h3>${otherItem}</h3><img src=${otherImgUrl} alt="a photo of an item"></div>`);
+    $("#added-items").append(`<div ondragstart="return onDragStart(event)" class="card"><h3>${otherItem}</h3><img src="assets/images/default.png" alt="a photo of an item"></div>`);
   });
 }
