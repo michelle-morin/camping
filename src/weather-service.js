ee72ed4c9f9a639131e845cf96893b8f996da817
Student <example@example.com>
2020-02-18 14:40:34 -0800

    remove console log

--- src/weather-service.js
+++ src/weather-service.js
@@ -2,7 +2,6 @@ export class WeatherService {
   async getWeatherByLoc(lat, lon) {
     try {
       let response = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.OPNW_API_KEY}`);
-      console.log(response);
       if (response.status === 200 && response.ok) {
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
