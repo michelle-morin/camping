541816c3aa67a11e752a9cf6db3a544e3cb25017
Michelle Morin <morin2m@gmail.com>
2020-02-19 19:00:21 -0800

    refactor function to run within initializePage and addCamper functions, in addition to onclick of Finalize Trip button

--- src/finalize.js
+++ src/finalize.js
@@ -1,19 +1,38 @@
+// export function finalizeTrip() {
+//   document.getElementById("save-trip").onclick = function() { 
+//     let campers = [];
+//     let camperItems = [];
+//     let camperArr = document.getElementsByClassName("parent");    
+//     for (let j = 1; j < camperArr.length - 1; j++) {
+//       campers.push(camperArr[j].id);     
+//       let itemsForCamper = camperArr[j].getElementsByClassName("box");
+//       let currentCamperItems = [];
+//       for (let i = 0; i < itemsForCamper.length; i++){
+//         currentCamperItems.push(itemsForCamper[i].id);
+//       }
+//       camperItems.push(currentCamperItems);
+//     }
+//     localStorage.setItem('storedcampers', JSON.stringify(campers));
+//     localStorage.setItem('storedCamperItems', JSON.stringify(camperItems));
+//   };
+// }
 
 export function finalizeTrip() {
-  document.getElementById("save-trip").onclick = function() { 
-    let campers = [];
-    let camperItems = [];
-    let camperArr = document.getElementsByClassName("parent");    
-    for (let j = 1; j < camperArr.length - 1; j++) {
-      campers.push(camperArr[j].id);     
-      let itemsForCamper = camperArr[j].getElementsByClassName("box");
-      let currentCamperItems = [];
-      for (let i = 0; i < itemsForCamper.length; i++){
-        currentCamperItems.push(itemsForCamper[i].id);
-      }
-      camperItems.push(currentCamperItems);
+  let campers = [];
+  let camperItems = [];
+  let camperArr = document.getElementsByClassName("parent");    
+  for (let j = 1; j < camperArr.length - 1; j++) {
+    campers.push(camperArr[j].id);     
+    let itemsForCamper = camperArr[j].getElementsByClassName("box");
+    let currentCamperItems = [];
+    for (let i = 0; i < itemsForCamper.length; i++){
+      currentCamperItems.push(itemsForCamper[i].id);
     }
-    localStorage.setItem('storedcampers', JSON.stringify(campers));
-    localStorage.setItem('storedCamperItems', JSON.stringify(camperItems));
+    camperItems.push(currentCamperItems);
+  }
+  localStorage.setItem('storedcampers', JSON.stringify(campers));
+  localStorage.setItem('storedCamperItems', JSON.stringify(camperItems));
+  document.getElementById("save-trip").onclick = function() { 
+    finalizeTrip();
   };
 }
