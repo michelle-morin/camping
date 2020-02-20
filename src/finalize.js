a257880d21b7cafe0bda286f14f7fa15f67366f3
Student <example@example.com>
2020-02-20 11:18:49 -0800

    remove commented code no longer being used

--- src/finalize.js
+++ src/finalize.js
@@ -1,22 +1,3 @@
-// export function finalizeTrip() {
-//   document.getElementById("save-trip").onclick = function() { 
-//     let campers = [];
-//     let camperItems = [];
-//     let camperArr = document.getElementsByClassName("parent");    
-//     for (let j = 1; j < camperArr.length - 1; j++) {
-//       campers.push(camperArr[j].id);     
-//       let itemsForCamper = camperArr[j].getElementsByClassName("box");
-//       let currentCamperItems = [];
-//       for (let i = 0; i < itemsForCamper.length; i++){
-//         currentCamperItems.push(itemsForCamper[i].id);
-//       }
-//       camperItems.push(currentCamperItems);
-//     }
-//     localStorage.setItem('storedcampers', JSON.stringify(campers));
-//     localStorage.setItem('storedCamperItems', JSON.stringify(camperItems));
-//   };
-// }
-
 export function finalizeTrip() {
   let campers = [];
   let camperItems = [];
